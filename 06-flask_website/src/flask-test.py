08811a136334e98fad7d38c049665658d06e5e1e
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-02-16 20:33:29 +0100

    Created 06-flask_website

---
+++ 06-flask_website/src/flask-test.py
@@ -0,0 +1,12 @@
+#!/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Drink more coffee RIGHT NOW!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
